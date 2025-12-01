d71662542cb6e07e42b883e70be63913e9ebfc88
crist <cristhianchulca49@gmail.com>
2025-12-01 14:12:37 +0100

    feat: add GenericMethods class with printGenericMethod() to print generic objects

---
+++ src/main/java/Level1/E2/GenericMethods.java
@@ -0,0 +1,7 @@
+package Level1.E2;
+
+public class GenericMethods {
+    public void printGenericMethod(Object object1, Object object2, Object object3){
+        System.out.println(object1.toString() + " " + object2.toString() + " " + object3.toString());
+    }
+}
