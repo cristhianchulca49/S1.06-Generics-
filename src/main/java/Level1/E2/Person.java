53284c15474f1bd3258b161526df5696f181c2a7
crist <cristhianchulca49@gmail.com>
2025-12-01 14:15:41 +0100

    feat: add Person Constructor

--- src/main/java/Level1/E2/Person.java
+++ src/main/java/Level1/E2/Person.java
@@ -5,6 +5,12 @@ public class Person {
     private String surname;
     private int age;
 
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return name + surname + age;
