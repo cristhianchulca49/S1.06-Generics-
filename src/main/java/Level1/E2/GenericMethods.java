e5eb6acbcb8465b4511cd45265b84618b14a512c
crist <cristhianchulca49@gmail.com>
2025-12-01 14:34:12 +0100

    refactor: replace Object with <T> for type safety

--- src/main/java/Level1/E2/GenericMethods.java
+++ src/main/java/Level1/E2/GenericMethods.java
@@ -1,7 +1,7 @@
 package Level1.E2;
 
 public class GenericMethods {
-    public void printGenericMethod(Object object1, Object object2, Object object3){
-        System.out.println(object1.toString() + " " + object2.toString() + " " + object3.toString());
+    public static <T> void printGenericMethod(T object1, T object2, T object3){
+        System.out.println(object1 + " " + object2 + " " + object3);
     }
 }
