48e535c8dc4d4e98e967523a64266066ab84a11c
crist <cristhianchulca49@gmail.com>
2025-12-01 13:47:05 +0100

    feat: add NoGenericMethods class with tree generic attributes and constructor

--- src/main/java/Level1/E1/NoGenericMethods.java
+++ src/main/java/Level1/E1/NoGenericMethods.java
@@ -1,7 +1,13 @@
 package Level1.E1;
 
-public class Main {
-    static void main() {
-        
+public class NoGenericMethods {
+    private Object object1;
+    private Object object2;
+    private Object object3;
+
+    public NoGenericMethods (Object object1, Object object2, Object object3) {
+        this.object1 = object1;
+        this.object2 = object2;
+        this.object3 = object3;
     }
 }
