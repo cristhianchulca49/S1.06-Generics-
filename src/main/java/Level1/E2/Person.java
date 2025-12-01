bc28de84d6134b8a41d2d8e8ac871ae2144f28d6
crist <cristhianchulca49@gmail.com>
2025-12-01 14:13:52 +0100

    feat: override toString to print name, surname and age

--- src/main/java/Level1/E2/Person.java
+++ src/main/java/Level1/E2/Person.java
@@ -4,4 +4,9 @@ public class Person {
     private String name;
     private String surname;
     private int age;
+
+    @Override
+    public String toString() {
+        return name + surname + age;
+    }
 }
