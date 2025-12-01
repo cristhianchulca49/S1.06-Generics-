86720913bf4da787f71cf10fc5a4cb22b7427ea6
crist <cristhianchulca49@gmail.com>
2025-12-01 13:47:28 +0100

    feat: add getters and setters

--- src/main/java/Level1/E1/NoGenericMethods.java
+++ src/main/java/Level1/E1/NoGenericMethods.java
@@ -10,4 +10,29 @@ public class NoGenericMethods {
         this.object2 = object2;
         this.object3 = object3;
     }
+    // setters
+    public void setObject1(Object object1) {
+        this.object1 = object1;
+    }
+
+    public void setObject2(Object object2) {
+        this.object2 = object2;
+    }
+
+    public void setObject3(Object object3) {
+        this.object3 = object3;
+    }
+
+    // getters
+    public Object getObj1() {
+        return object1;
+    }
+
+    public Object getObj2() {
+        return object2;
+    }
+
+    public Object getObj3() {
+        return object3;
+    }
 }
